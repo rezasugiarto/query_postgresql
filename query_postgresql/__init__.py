18b1ee9bb99606366da8191172431bd1d5032e36
Reza <reza16.sugiarto@gmail.com>
2019-10-10 17:41:59 +0700

    add file

---
+++ query_postgresql/__init__.py
@@ -0,0 +1,1 @@
+from query_postgresql import query_postgres
---
+++ query_postgresql/query_postgresql.py
@@ -0,0 +1,38 @@
+import psycopg2
+import pandas as pd
+
+class query_postgres:
+    def __init__(self,hostname,username,password,database,port):
+        self.hostname=hostname
+        self.username=username
+        self.password=password
+        self.database=database
+        self.port=port
+
+    
+    def query_todf(self,query):
+        conn=psycopg2.connect(host=self.hostname,user=self.username,password=self.password,port=self.port,dbname=self.database)
+        cursor=conn.cursor()
+        cursor.execute(query)
+        result=cursor.fetchall()
+        df=pd.DataFrame(result)
+        col_name=[name[0] for name in cursor.description]
+        df.columns=col_name
+        return df
+    
+    def query_iteration(self,query,iteration):
+        conn=psycopg2.connect(host=self.hostname,user=self.username,password=self.password,port=self.port,dbname=self.database)
+        cursor=conn.cursor()
+        cursor.execute(query.format(iteration))
+        result=cursor.fetchall()
+        df=pd.DataFrame(result)
+        col_name=[name[0] for name in cursor.description]
+        df.columns=col_name
+        return df
+    
+    def query_tovar(self,query):
+        conn=psycopg2.connect(host=self.hostname,user=self.username,password=self.password,port=self.port,dbname=self.database)
+        cursor=conn.cursor()
+        cursor.execute(query)
+        result=cursor.fetchall()
+        return result
