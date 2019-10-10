e3cd515cd5b910a89f89a403636c4d38f3960c22
Reza Sugiarto <reza16.sugiarto@gmail.com>
2019-10-10 18:15:10 +0700

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from distutils.core import setup
 setup(
-  name = 'query_postgresql',         # How you named your package folder (MyLib)
-  packages = ['query_postgresql'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Querying using Postgresql made easy',   # Give a short description about your library
-  author = 'Reza',                   # Type in your name
-  author_email = 'reza16.sugiarto@gmail.com',      # Type in your E-Mail
-  url = https://github.com/rezasugiarto/query_postgresql',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['postgresql'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'query_postgresql',         
+  packages = ['query_postgresql'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Querying using Postgresql made easy',   
+  author = 'Reza',                   
+  author_email = 'reza16.sugiarto@gmail.com',      
+  url = https://github.com/rezasugiarto/query_postgresql',   
+  download_url = 'https://github.com/rezasugiarto/query_postgresql/archive/0.1.tar.gz',    
+  keywords = ['postgresql'],   
+  install_requires=[           
           'pandas',
           'psycopg2',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers/Me',      # Define that your audience are developers
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers/Me',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
